da62ff7a9311b081bd1349d4fa4f48f8ad2ad231
jmckinny <justinmckinney.md@gmail.com>
2021-12-02 12:17:49 -0500

    day1 complete

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,18 +4,36 @@ use std::io::{prelude::*, BufReader};
 
 fn main() {
     let data = read_data("data.txt");
-    let result = larger_than_prev(data);
-    println!("{:?}", result);
+    println!("Part 1 = {:?}", larger_than_prev(&data));
+    println!("Part 2 = {:?}", window_version(&data));
 }
 
-fn larger_than_prev(data: Vec<i32>) -> i32 {
+fn larger_than_prev(data: &[i32]) -> i32 {
     let mut count = 0;
     let mut prev = i32::MAX;
-    for num in data.into_iter(){
+    for num in data {
         if num.cmp(&prev) == cmp::Ordering::Greater {
             count += 1
         };
-        prev = num;
+        prev = *num;
+    }
+    count
+}
+
+fn window_version(data: &[i32]) -> i32 {
+    let mut count = 0;
+
+    let mut cur: i32;
+    let mut window = [data[0], data[1], data[2]];
+    let mut prev = window.iter().sum();
+    for (index, num) in data.iter().skip(3).enumerate() {
+        window[index % 3] = *num;
+        cur = window.iter().sum();
+
+        if cur.cmp(&prev) == cmp::Ordering::Greater {
+            count += 1;
+        }
+        prev = cur;
     }
     count
 }
