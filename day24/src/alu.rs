e1a4ea429af76c43a8e99d75380ec979a6ab8cd3
Justin McKinney <jmckinny@terpmail.umd.edu>
2021-12-24 22:32:29 -0500

    brute force doesnt work

--- day24/src/alu.rs
+++ day24/src/alu.rs
@@ -1,96 +1,113 @@
 use core::panic;
 
-#[derive(Debug,Clone)]
-pub enum Instruction{
+#[derive(Debug, Clone)]
+pub enum Instruction {
     Inp(Register),
-    Add{a:Register,b:Register},
-    Mul{a:Register,b:Register},
-    Div{a:Register,b:Register},
-    Mod{a:Register,b:Register},
-    Eql{a: Register, b:Register}
+    Add { a: Register, b: Register },
+    Mul { a: Register, b: Register },
+    Div { a: Register, b: Register },
+    Mod { a: Register, b: Register },
+    Eql { a: Register, b: Register },
 }
-#[derive(Debug,Clone)]
-pub enum Register{
+#[derive(Debug, Clone)]
+pub enum Register {
     W,
     X,
     Y,
     Z,
-    Num(i32)
+    Num(i64),
 }
 
 #[derive(Debug)]
-pub struct VM{
+pub struct VM {
     instructions: Vec<Instruction>,
-    w:i32,
-    x:i32,
-    y:i32,
-    pub z:i32
+    w: i64,
+    x: i64,
+    y: i64,
+    pub z: i64,
 }
 
-impl VM{
-    pub fn new(instructions:Vec<Instruction>) -> Self{
-        VM{
+impl VM {
+    pub fn new(instructions: Vec<Instruction>) -> Self {
+        VM {
             instructions,
-            w:0,
-            x:0,
-            y:0,
-            z:0
+            w: 0,
+            x: 0,
+            y: 0,
+            z: 0,
         }
     }
-    pub fn run(&mut self){
-        for i in 0..self.instructions.len(){
-            match &self.instructions[i].clone(){
-                Instruction::Inp(reg)=> {
-                    let val;
-                    let mut buf = String::new();
-                    std::io::stdin().read_line(&mut buf).unwrap();
-                    let str = buf.strip_suffix('\n').unwrap();
-                    val = str.to_string().parse::<i32>().unwrap();
-                    println!("Read '{}'", val);
+    pub fn run(&mut self, input: Option<&[u32]>) {
+        let mut count = 0;
+        for i in 0..self.instructions.len() {
+            match &self.instructions[i].clone() {
+                Instruction::Inp(reg) => {
+                    let val:i64;
+                    if let Some(nums) = input {
+                        val = nums[count] as i64;
+                        count += 1;
+                    } else {
+                        let mut buf = String::new();
+                        std::io::stdin().read_line(&mut buf).unwrap();
+                        let str = buf.strip_suffix('\n').unwrap();
+                        val = str.to_string().parse::<i64>().unwrap();
+                    }
                     self.store_val(&reg.clone(), val);
-                },
-                Instruction::Add{a,b} =>{
+                }
+                Instruction::Add { a, b } => {
                     let val = self.get_val(a) + self.get_val(b);
                     self.store_val(a, val);
-                },
-                Instruction::Mul { a, b } =>{
+                }
+                Instruction::Mul { a, b } => {
                     let val = self.get_val(a) * self.get_val(b);
                     self.store_val(a, val);
-                },
-                Instruction::Div{a,b} => {
+                }
+                Instruction::Div { a, b } => {
                     let val = self.get_val(a) / self.get_val(b);
                     self.store_val(a, val);
-                },
-                Instruction::Mod{a,b} =>{
+                }
+                Instruction::Mod { a, b } => {
                     let val = self.get_val(a) % self.get_val(b);
                     self.store_val(a, val);
-                },
-                Instruction::Eql{a,b} => {
+                }
+                Instruction::Eql { a, b } => {
                     let val = self.get_val(a) == self.get_val(b);
-                    if val {self.store_val(a, 1)} else {self.store_val(a, 0)};
+                    if val {
+                        self.store_val(a, 1)
+                    } else {
+                        self.store_val(a, 0)
+                    };
                 }
             };
         }
     }
 
-    fn store_val(&mut self, reg:&Register, val: i32){
-        match reg{
-            Register::W => {self.w = val},
-            Register::X => {self.x = val},
-            Register::Y => {self.y = val},
-            Register::Z => {self.z = val},
-            _ => {panic!("Failed to store reg!")}
+    fn store_val(&mut self, reg: &Register, val: i64) {
+        match reg {
+            Register::W => self.w = val,
+            Register::X => self.x = val,
+            Register::Y => self.y = val,
+            Register::Z => self.z = val,
+            _ => {
+                panic!("Failed to store reg!")
+            }
         };
     }
 
-    pub fn get_val(&self, reg:&Register) -> i32{
-        match reg{
-            Register::W => {self.w},
-            Register::X => {self.x},
-            Register::Y => {self.y},
-            Register::Z => {self.z},
+    pub fn get_val(&self, reg: &Register) -> i64 {
+        match reg {
+            Register::W => self.w,
+            Register::X => self.x,
+            Register::Y => self.y,
+            Register::Z => self.z,
             Register::Num(x) => *x,
         }
     }
 
+    pub fn reset(&mut self){
+        self.w = 0;
+        self.x = 0;
+        self.y = 0;
+        self.z = 0;
+    }
 }
--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,82 +1,79 @@
-use std::{io::{BufRead, BufReader}, fs::File};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 mod alu;
 fn main() {
     let data = read_data("data.txt");
     let mut alu = alu::VM::new(data);
-    alu.run();
-    println!("{:?}", alu.z);
+    for i in (0..99999999999999_usize).rev(){
+        println!("Trying {}", i);
+        let digits:Vec<u32> = i.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+        alu.run(Some(&digits));
+        if alu.z == 0{
+            println!("{}",i);
+            break;
+        }
+        alu.reset();
+    }
 }
 
-
-fn read_data(filename:&str) -> Vec<alu::Instruction>{
+fn read_data(filename: &str) -> Vec<alu::Instruction> {
     let mut result = Vec::new();
     let reader = BufReader::new(File::open(filename).unwrap());
-    for line in reader.lines(){
+    for line in reader.lines() {
         let unwrapped = line.unwrap();
-        let split:Vec<&str> = unwrapped.split_whitespace().collect();
+        let split: Vec<&str> = unwrapped.split_whitespace().collect();
         //First 3 are instruction next is reg, last is variable
-        match split[0]{
+        match split[0] {
             "inp" => {
                 let reg = str_to_reg(split[1]);
                 result.push(alu::Instruction::Inp(reg));
-            },
+            }
             "add" => {
                 let reg1 = str_to_reg(split[1]);
                 let reg2 = str_to_reg(split[2]);
-                result.push(alu::Instruction::Add{
-                    a:reg1,
-                    b:reg2
-                });
-            },
+                result.push(alu::Instruction::Add { a: reg1, b: reg2 });
+            }
             "mul" => {
                 let reg1 = str_to_reg(split[1]);
                 let reg2 = str_to_reg(split[2]);
-                result.push(alu::Instruction::Mul{
-                    a:reg1,
-                    b:reg2
-                });
-            },
+                result.push(alu::Instruction::Mul { a: reg1, b: reg2 });
+            }
             "div" => {
                 let reg1 = str_to_reg(split[1]);
                 let reg2 = str_to_reg(split[2]);
-                result.push(alu::Instruction::Div{
-                    a:reg1,
-                    b:reg2
-                });
-                
-            },
-            "mod" =>{
-                let reg1 = str_to_reg(split[1]);
-                let reg2 = str_to_reg(split[2]);
-                result.push(alu::Instruction::Mod{
-                    a:reg1,
-                    b:reg2
-                });
-
-            },
-            "eql" =>{
-                let reg1 = str_to_reg(split[1]);
-                let reg2 = str_to_reg(split[2]);
-                result.push(alu::Instruction::Eql{
-                    a:reg1,
-                    b:reg2
-                });
-
+                result.push(alu::Instruction::Div { a: reg1, b: reg2 });
             }
-            _ => {panic!("Invalid command")}
+            "mod" => {
+                let reg1 = str_to_reg(split[1]);
+                let reg2 = str_to_reg(split[2]);
+                result.push(alu::Instruction::Mod { a: reg1, b: reg2 });
+            }
+            "eql" => {
+                let reg1 = str_to_reg(split[1]);
+                let reg2 = str_to_reg(split[2]);
+                result.push(alu::Instruction::Eql { a: reg1, b: reg2 });
+            }
+            _ => {
+                panic!("Invalid command")
+            }
         }
     }
 
     result
 }
 
-fn str_to_reg(reg :&str) -> alu::Register{
-    match reg{
-        "w" => {alu::Register::W},
-        "x" =>{alu::Register::X},
-        "y"=>{alu::Register::Y},
-        "z"=>{alu::Register::Z},
-        x => {alu::Register::Num(x.parse().unwrap())}
+fn str_to_reg(reg: &str) -> alu::Register {
+    match reg {
+        "w" => alu::Register::W,
+        "x" => alu::Register::X,
+        "y" => alu::Register::Y,
+        "z" => alu::Register::Z,
+        x => alu::Register::Num(x.parse().unwrap()),
     }
 }
